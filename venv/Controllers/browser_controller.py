18b93212019a571a5c778acde5476013f7c2470e
Abraham <abrixa12@gmail.com>
2024-05-29 10:54:03 -0500

    New funcionality

--- venv/Controllers/browser_controller.py
+++ venv/Controllers/browser_controller.py
@@ -6,49 +6,44 @@ from Interfaces.browser_interface import Alone
 from selenium.common.exceptions import NoSuchElementException
 
 class Browser_Controller:
-    def find_atribute(self, browser: str, page: str, selector: str):
+    async def find_atribute(self, browser: Annotated[str, Form()], page: Annotated[str, Form()], selector: Annotated[str, Form()]):
         browser_instance = selenium_browser(browser)
         browser_instance.get(page)
 
-
         try:
             element = browser_instance.find_element(By.CSS_SELECTOR, selector)
-            browser_instance.quit()
             return Alone(
                 text=element.text,
                 id=element.get_attribute('id'),
                 className=element.get_attribute('class')
             )
         except NoSuchElementException as e:
-            browser_instance.quit()
             raise HTTPException(
                 status_code=404,
                 detail="no such element"
             )
+        finally:
+            browser_instance.quit()
 
 
-
-    def all_tags(self, browser: str, page: str, tag: str):
+    async def all_tags(self, browser: str, page: str, tag: str):
         browser_instance = selenium_browser(browser)
         browser_instance.get(page)
 
-        tags: Alone = list()
+        tags = list()
 
         try:
-            elements = browser_instance.find_elements(By.TAG_NAME, 'p')
+            elements = browser_instance.find_elements(By.TAG_NAME, tag)
 
             for element in elements:
-                tags.append(
-                    Alone(
-                    text="",
-                    id="element.get_attribute('id')",
-                    className="element.get_attribute('class')"
-                    )
+                append = Alone(
+                    text=element.text,
+                    id=element.get_attribute('id'),
+                    className=element.get_attribute('class')
                 )
+                tags.append(append)
 
-            browser_instance.quit()
-
-            return  ""
+            return  tags
 
         except NoSuchElementException as e:
             browser_instance.quit()
@@ -56,5 +51,6 @@ class Browser_Controller:
                 status_code=404,
                 detail="no such element"
             )
-
+        finally:
+            browser_instance.quit()
 
--- venv/Routes/browser_route.py
+++ venv/Routes/browser_route.py
@@ -4,12 +4,12 @@ from Controllers.browser_controller import Browser_Controller
 
 router = APIRouter()
 
-browser_controller = Browser_Controller()
+controller = Browser_Controller()
 
 @router.get("/scraping/atribute/")
-async def scraping_atribute(browser: Annotated[str, Form()], page: Annotated[str, Form()], selector: Annotated[str, Form()]):
-    browser_controller.find_atribute(browser, page, selector)
+async def scraping_atribute(browser: str, page: str, selector: str):
+    return await controller.find_atribute(browser, page, selector)
 
 @router.get("/scraping/all/tags/")
-async def scraping_tangs(browser: Annotated[str, Form()], page: Annotated[str, Form()], tag: Annotated[str, Form()]):
-    browser_controller.all_tags(browser, page, tag)
+async def scraping_tangs(browser: str, page: str, tag: str):
+    return await controller.all_tags(browser, page, tag)
